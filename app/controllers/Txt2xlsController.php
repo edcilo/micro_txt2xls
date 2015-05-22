4d6d94a14455535603cc5a21e1376f8d4b43aebf
edcilo <edcilop@gmail.com>
2015-05-22 14:29:18 -0500

    dejando que github haga lo que quiera

--- app/controllers/Txt2xlsController.php
+++ app/controllers/Txt2xlsController.php
@@ -1,5 +1,7 @@
 <?php
 
+header('Content-Type: text/html; charset=ISO-8859-1');
+
 class Txt2xlsController extends BaseController {
 
     public function index()
@@ -11,7 +13,7 @@ class Txt2xlsController extends BaseController {
     {
         $data = [];
         $file = Input::file('txt');
-        $name = $file->getClientOriginalName();
+        $name = $this->clean($file->getClientOriginalName());
 
         $file->move('txt_temp/', $name);
 
@@ -24,6 +26,11 @@ class Txt2xlsController extends BaseController {
         }
 
         fclose($file);
+
+        foreach ($data as $key => $value) {
+            $value = utf8_encode($value);
+            $data[$key] = $this->clean($value);
+        }
 
         Excel::create($name, function($excel) use ($name, $data) {
 
@@ -45,43 +52,44 @@ class Txt2xlsController extends BaseController {
             });
 
             $excel->setActiveSheetIndex(1)
-                ->setCellValue('A1', 'Cliente')
-                ->setCellValue('A2', $data[1])
-                ->setCellValue('B1', 'RFC')
-                ->setCellValue('B2', $data[2])
-                ->setCellValue('C1', 'Calle')
-                ->setCellValue('C2', $data[3])
-                ->setCellValue('D1', 'Num_Ext')
-                ->setCellValue('E1', 'Num_Int')
-                ->setCellValue('F1', 'Colonia')
-                ->setCellValue('F2', $data[4])
-                ->setCellValue('G1', 'Población')
-                ->setCellValue('G2', $data[5])
-                ->setCellValue('H1', 'Municipio')
-                ->setCellValue('I1', 'Estado')
-                ->setCellValue('J1', 'CP')
-                ->setCellValue('J2', $data[6])
-                ->setCellValue('K1', 'Cta_Predial')
-                ->setCellValue('L1', 'IVA')
-                ->setCellValue('M1', 'IVA_Ret')
-                ->setCellValue('N1', 'ISR_Ret')
-                ->setCellValue('O1', 'Impuesto_local')
-                ->setCellValue('P1', 'IVA_Exento')
-                ->setCellValue('Q1', 'Operacion_publico_general');
+                    ->cell('A1', 'Cliente')
+                    ->cell('A2', $data[1])
+                    ->cell('B1', 'RFC')
+                    ->cell('B2', $data[2])
+                    ->cell('C1', 'Calle')
+                    ->cell('C2', $data[3])
+                    ->cell('D1', 'Num_Ext')
+                    ->cell('E1', 'Num_Int')
+                    ->cell('F1', 'Colonia')
+                    ->cell('F2', $data[4])
+                    ->cell('G1', 'Población')
+                    ->cell('G2', $data[5])
+                    ->cell('H1', 'Municipio')
+                    ->cell('I1', 'Estado')
+                    ->cell('J1', 'CP')
+                    ->cell('J2', $data[6])
+                    ->cell('K1', 'Cta_Predial')
+                    ->cell('L1', 'IVA')
+                    ->cell('M1', 'IVA_Ret')
+                    ->cell('N1', 'ISR_Ret')
+                    ->cell('O1', 'Impuesto_local')
+                    ->cell('P1', 'IVA_Exento')
+                    ->cell('Q1', 'Operacion_publico_general');
 
             $excel->setActiveSheetIndex(2)
-                ->setCellvalue('A1', 'Cantidad')
-                ->setCellvalue('B1', 'Producto')
-                ->setCellvalue('C1', 'Unidad')
-                ->setCellvalue('D1', 'Precio_Unitario');
+                ->cell('A1', 'Cantidad')
+                ->cell('B1', 'Producto')
+                ->cell('C1', 'Unidad')
+                ->cell('D1', 'Precio_Unitario');
 
+            $sheet = $excel->setActiveSheetIndex(2);
             $row = 2;
             for ( $i = 9; $i < count($data); $i+=7 )
             {
                 $line = $i;
 
                 // cantidad                         9
-                $excel->setCellvalue("A$row", $data[$line]);
+                $sheet->cell("A$row", $data[$line]);
 
                 // codigo de barras                 10
                 $line++;
@@ -107,14 +115,14 @@ class Txt2xlsController extends BaseController {
                     }
                     $description .= $list;
                 }
-                $excel->setCellvalue("B$row", $description);
+                $sheet->cell("B$row", $description);
 
                 // unidad
-                $excel->setCellvalue("C$row", 'PIEZA');
+                $sheet->cell("C$row", 'PIEZA');
 
                 // costo unitario                   14
                 $line++;
-                $excel->setCellvalue("D$row", $data[$line]);
+                $sheet->cell("D$row", $data[$line]);
 
                 // linea en blanco                  15
                 $line += 1;
@@ -124,6 +132,64 @@ class Txt2xlsController extends BaseController {
             $excel->setActiveSheetIndex(0);
 
         })->download('xlsx');
+    }
+
+    public function clean($string)
+    {
+
+        $string = trim($string);
+
+        $string = str_replace(
+            array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+            array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+            $string
+        );
+
+        $string = str_replace(
+            array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+            array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+            $string
+        );
+
+        $string = str_replace(
+            array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+            array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+            $string
+        );
+
+        $string = str_replace(
+            array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+            array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+            $string
+        );
+
+        $string = str_replace(
+            array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+            array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+            $string
+        );
+
+        $string = str_replace(
+            array('ñ', 'Ñ', 'ç', 'Ç'),
+            array('n', 'N', 'c', 'C',),
+            $string
+        );
+
+        //Esta parte se encarga de eliminar cualquier caracter extraño
+        $string = str_replace(
+            array("\\", "¨", "º", "-", "~",
+                "#", "@", "|", "!", "\"",
+                "·", "$", "%", "&", "/",
+                "(", ")", "?", "'", "¡",
+                "¿", "[", "^", "`", "]",
+                "+", "}", "{", "¨", "´",
+                ">", "< ", ";", ",", ":",
+                ".", " ", "�"),
+            '',
+            $string
+        );
+
+        return $string;
     }
 
 } 
